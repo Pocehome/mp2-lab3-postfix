3f07fac97290c48c3f2728b30fad8467e7f74489
Pocehome <74858692+Pocehome@users.noreply.github.com>
2023-11-27 12:23:46 +0300

    Update sample_postfix.cpp
    classwork 2

--- Сысоев/base/sample_postfix.cpp
+++ Сысоев/base/sample_postfix.cpp
@@ -1,30 +1,29 @@
-#include <iostream>
-#include <string>
 #include "postfix.h"
 
-using namespace std;
+int main() {
+	std::string expression = "((1+3)/2)-(2+4)/2";
+	double res = 0.0;
 
-int main()
-{
-  string expression;
-  TPostfix postfix;
-  double res;
+	//std::cout << "Enter expression: ";
+	//std::getline(std::cin, expression);
+	TArithmeticExpression postfix(expression);
+	std::cout << expression << std::endl;
+	std::cout << "Expression: " << postfix.getInfix() << std::endl;
+	std::cout << "Postfix: " << postfix.getPostfix() << std::endl;
+	res = postfix.calculate();
+	std::cout << "Result: " << res << std::endl;
 
-  setlocale(LC_ALL, "Russian");
-  //cout << "Введите арифметическое выражение: ";
-  //cin >> expression;
-  //cout << expression << endl;
-  cout << "Арифметическое выражение: " << postfix.GetInfix() << endl;
-  postfix.ToPostfix();
-  cout << "Постфиксная форма: " << postfix.GetPostfix() << endl;
-  res = postfix.Calculate();
-  cout << res << endl;
-
-  return 0;
+	return 0;
 }
 
-
-//int main() {
-//	int a[5] = {1, 2, 3, 4, 5};
-//	TStack stack(a);
+//void main() {
+//	int arr[] = { 1, 2, 3, 4, 5 };
+//	TStack<int> st(3);
+//	st.push(3);
+//	st.push(6);
+//	st.push(1);
+//	for (size_t i = 0; i < 3; i++) {
+//		std::cout << st.top() << std::endl;
+//		st.pop();
+//	}
 //}
